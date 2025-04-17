9fbd0f546c13cefb41e046dd9cb032532419e5b1
Hoang Tien Lang <langhoangtien@gmail.com>
2025-04-17 11:47:31 +0700

    dsfdsf

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
       s.parentNode.insertBefore(t, s)
     }(window, document, 'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '3741128446126361');
+    fbq('init', '1393158791704032');
     fbq('track', 'PageView');
           `}
         </Script>
@@ -64,7 +64,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=3741128446126361&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1393158791704032&ev=PageView&noscript=1"
           />
         </noscript>
         <CartProvider>{children}</CartProvider>
